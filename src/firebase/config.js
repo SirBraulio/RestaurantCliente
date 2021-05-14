8dcf04cf99542c4f00b0108c6fd35cb650e9d65a
SirBraulio <68666064+SirBraulio@users.noreply.github.com>
2021-05-14 15:24:23 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDBxm3P0F609yaYgDE_NPJdyD1SZsIVV0U",
-    authDomain: "restaurant-66aca.firebaseapp.com",
-    projectId: "restaurant-66aca",
-    storageBucket: "restaurant-66aca.appspot.com",
-    messagingSenderId: "262951277234",
-    appId: "1:262951277234:web:d68522560966a97df96fab",
-    measurementId: "G-E60YQ2ZLWD"
+    
   };
 
   export default firebaseConfig;
